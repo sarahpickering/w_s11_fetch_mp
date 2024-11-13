095cd9f2b00e6ea070c9495669e582c0dc645d17
Sarah Pickering <sarahpsellsaz@gmail.com>
2024-11-12 22:31:49 -0700

    create callbacks for list functions, create edit and delete dog functions

--- frontend/components/DogsList.js
+++ frontend/components/DogsList.js
@@ -1,18 +1,27 @@
 import React from 'react'
 
-export default function DogsList() {
+export default function DogsList({ dogs, getDogs, setCurrentDog }) {
+  const editDog = id => {
+    console.log('...editing')
+  } 
+  const deleteDog = id => {
+    console.log('...deleting')
+  }
   return (
     <div>
       <h2>Dogs Shelter</h2>
       <ul>
-        <li>
-          Fido, Bulldog, NOT adopted
-          <div>
-            <button>Edit</button>
-            <button>Delete</button>
-          </div>
-        </li>
+        {
+        dogs.map(dog => (
+          <li key={dog.id}>
+            {dog.name}, {dog.breed}, {dog.adopted ? '' : 'NOT'}adopted
+            <div>
+              <button>Edit</button>
+              <button>Delete</button>
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
-  )
+  );
 }
