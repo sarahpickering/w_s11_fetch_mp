f4e1ce83c1892a09f4a8d38fe374b3d982a7f512
Sarah Pickering <sarahpsellsaz@gmail.com>
2024-11-12 22:15:29 -0700

    Initialize state and components, implement fetch function, fetch data and set state

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import { Routes, Route, NavLink } from 'react-router-dom'
 import DogForm from './DogForm'
 import DogsList from './DogsList'
 
 export default function App() {
+  const [dogs, setDogs] = useState([]);
+  const [currentDogId, setCurrentDog] = useState(null);
+
+  useEffect(() => { getDogs() }, [])
+
+  const getDogs = () => {
+    fetch('/api/dogs')
+      .then(res => {
+        if (!res.ok) throw new Error('Problem getting dogs')
+          return res.json()
+      })
+      .then(setDogs)
+  }
   return (
     <div>
       <nav>
@@ -11,9 +24,21 @@ export default function App() {
         <NavLink to="/form">Form</NavLink>
       </nav>
       <Routes>
-        <Route path="/" element={<DogsList />} />
-        <Route path="/form" element={<DogForm />} />
+        <Route path="/" element={<DogsList 
+        dogs={dogs}
+        getDogs={getDogs}
+        setCurrentDog={setCurrentDog} />} />
+        <Route
+          path="/form"
+          element={
+            <DogForm
+              dog={currentDogId && dogs.find((d) => d.id == currentDogId)}
+              getDogs={getDogs}
+              reset={() => setCurrentDog(null)}
+            />
+          }
+        />
       </Routes>
     </div>
-  )
+  );
 }
